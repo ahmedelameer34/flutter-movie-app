7afc60c171e908bf4b0dba8dbfc3dcbc33bdcd29
Ahmed Badawy Mohamed El Amir <amirbadawy68@gmail.com>
2023-05-22 07:37:55 +0300

    bloc_builder

--- lib/movies/presentation/screens/movies_screen.dart
+++ lib/movies/presentation/screens/movies_screen.dart
@@ -20,96 +20,92 @@ class MoviesScreen extends StatelessWidget {
         ..add(MoviesGetNowPlayingEvent())
         ..add(MoviesGetPopularEvent())
         ..add(MoviesGetTopRatedEvent()),
-      child: BlocBuilder<MoviesBloc, MoviesStates>(
-        builder: (context, state) {
-          return Scaffold(
-            backgroundColor: Colors.blueGrey.shade900,
-            body: SingleChildScrollView(
-              key: const Key('movieScrollView'),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const NowPlayingComponents(),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Popular",
-                          style: GoogleFonts.poppins(
-                            fontSize: 19,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white,
-                            letterSpacing: 0.15,
-                          ),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            /// TODO : NAVIGATION TO POPULAR SCREEN
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              children: const [
-                                Text('See More',
-                                    style: TextStyle(color: Colors.white)),
-                                Icon(Icons.arrow_forward_ios,
-                                    size: 16.0, color: Colors.white)
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
+      child: Scaffold(
+        backgroundColor: Colors.blueGrey.shade900,
+        body: SingleChildScrollView(
+          key: const Key('movieScrollView'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const NowPlayingComponents(),
+              Container(
+                margin: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Popular",
+                      style: GoogleFonts.poppins(
+                        fontSize: 19,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                        letterSpacing: 0.15,
+                      ),
                     ),
-                  ),
-                  const TopRatedComponents(),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(
-                      16.0,
-                      24.0,
-                      16.0,
-                      8.0,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Top Rated",
-                          style: GoogleFonts.poppins(
-                              fontSize: 19,
-                              fontWeight: FontWeight.w500,
-                              letterSpacing: 0.15,
-                              color: Colors.white),
+                    InkWell(
+                      onTap: () {
+                        /// TODO : NAVIGATION TO POPULAR SCREEN
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: const [
+                            Text('See More',
+                                style: TextStyle(color: Colors.white)),
+                            Icon(Icons.arrow_forward_ios,
+                                size: 16.0, color: Colors.white)
+                          ],
                         ),
-                        InkWell(
-                          onTap: () {
-                            /// TODO : NAVIGATION TO Top Rated Movies Screen
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              children: const [
-                                Text(
-                                  'See More',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                Icon(Icons.arrow_forward_ios,
-                                    size: 16.0, color: Colors.white)
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
-                  const PopularComponents(),
-                  const SizedBox(height: 50.0),
-                ],
+                  ],
+                ),
               ),
-            ),
-          );
-        },
+              const TopRatedComponents(),
+              Container(
+                margin: const EdgeInsets.fromLTRB(
+                  16.0,
+                  24.0,
+                  16.0,
+                  8.0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Top Rated",
+                      style: GoogleFonts.poppins(
+                          fontSize: 19,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: 0.15,
+                          color: Colors.white),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        /// TODO : NAVIGATION TO Top Rated Movies Screen
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: const [
+                            Text(
+                              'See More',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Icon(Icons.arrow_forward_ios,
+                                size: 16.0, color: Colors.white)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const PopularComponents(),
+              const SizedBox(height: 50.0),
+            ],
+          ),
+        ),
       ),
     );
   }
